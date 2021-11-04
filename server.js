a983d7b6be1a5953c78f726597ca1e8b7b9b459b
Julia <222596@edu.p.lodz.pl>
2021-11-04 13:15:57 +0100

    working with fetch

--- server.js
+++ server.js
@@ -10,3 +10,25 @@ app.listen(port, host, (error) => {
     console.log(`Server started at port ${port}`);
   }
 });
+
+app.get("/format", async (req, res) => {
+  const format = req.query.format;
+  const text = req.query.text;
+  if (format === "json") {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/CapitalLetters?text=${text}`
+      );
+      const message = await response.json();
+      console.log(message);
+    } catch (error) {
+      console.error(error);
+    }
+  } else if (format === "xml") {
+  } else if (format === "csv") {
+  } else {
+    return res.status(400).send("Incorrect format");
+  }
+
+  return res.status(200).send("success");
+});
